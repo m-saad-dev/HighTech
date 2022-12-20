abc571c5b3b31544d792e96724137ca6561cca02
m-saad-dev <stm.legend@gmail.com>
2022-12-20 14:20:13 +0200

    save work until dropzone

---
+++ public/assets/admin/summernote-editor.js
@@ -0,0 +1,21 @@
+        editors = $('.summernote');
+        for (editor of editors){
+            console.log(editor.value)
+
+            let summerNote = $('#'+editor.id).summernote({
+                placeholder: '',
+                lang: 'en-US',
+                tabsize: 2,
+                // height: 120,
+                toolbar: [
+                    ['style', ['style']],
+                    ['font', ['bold', 'underline', 'clear']],
+                    ['color', ['color']],
+                    ['para', ['ul', 'ol', 'paragraph']],
+                    ['table', ['table']],
+                    ['insert', ['link', 'picture', 'video']],
+                    ['view', ['fullscreen', 'codeview', 'help']]
+                ]
+            });
+            summerNote.summernote('code', editor.getAttribute('kt-data'));
+        }
