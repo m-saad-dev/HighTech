dd8e0ab1698267cc80911921b656999d084be46c
m-saad-dev <stm.legend@gmail.com>
2023-01-08 10:05:42 +0200

    changes

--- resources/views/website/blog.blade.php
+++ resources/views/website/blog.blade.php
@@ -9,18 +9,22 @@
             <hr />
             <div class="details col-lg-12 innerall">
                 @foreach($blog as $article)
-                    <a href="{{route('website.articles.show', ['article' => $article->id])}}">
-                        <div class="row mdwna inneermad">
-                            <div class="col-lg-6 imgtop"><img class="img-fluid" src="{{$article->getFirstMedia('icon') ? $article->getFirstMedia('icon')->getFullUrl() : asset('assets/website/img/hover3.png')}}" alt="المدونة"></div>
 
-                            <div class="col-lg-6">
+                    <div class="row mdwna inneermad">
+
+                        <div class="col-lg-6 imgtop"><img class="img-fluid" src="{{$article->getFirstMedia('icon') ? $article->getFirstMedia('icon')->getFullUrl() : asset('assets/website/img/hover3.png')}}" alt="المدونة"></div>
+
+                        <div class="col-lg-6">
+                            <a href="{{route('website.articles.show', ['article' => $article->id])}}">
                                 <div class="col  proj allblo">
                                     <div class="col first bl1">{{$article->translate('ar')->title}}</div>
                                     <div class="col first bl2">{{$article->translate('ar')->sub_title}}</div>
                                 </div>
-                            </div>
+                            </a>
                         </div>
-                    </a>
+
+                        <div class="col secondr"><a href="{{route('website.articles.show', ['article' => $article])}}">أكمل القراءة</a></div>
+                    </div>
                 @endforeach
                 <div class=" col-lg-12 m-0 allbut">
                     <button onclick="location.href='https://www.blog.hightech.com/';" class="serv mdwna">أنقر للمزيد من المقالات</button>
